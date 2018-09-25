add7c98dc0b8a3ae02d5252916d55bdd5ee0b1a4
clc80 <clc_80@yahoo.com>
2018-09-24 19:56:41 -0700

    Finished app

--- RandomStartUp/ViewController.swift
+++ RandomStartUp/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var startup = ["Make School", "Uber", "Netflix", "Facebook", "Google", "Kickstarter", "Spotify", "Airbnb", "Snapchat", "YouTube", "Dropbox", "Amazon", "Craigslist", "Tinder", "Instagram", "Tesla", "Twitter", "SpaceX"];
+    var subject = ["Dogs", "Books", "Gamers", "Star Wars", "Bitcoin", "Goats", "Zombies", "Rich People", "Swimmers", "Florida", "Vampires", "Dragons", "Internet of Things", "Mars", "Cryptocurrency", "Mosquito Repellent", "Fidget Spinners", "Sun Screen", "Water Bottles", "Lost Travelers", "Superheroes"];
+    
+    @IBOutlet weak var startUpLbl: UILabel!
+    @IBOutlet weak var subjectLbl: UILabel!
+    @IBOutlet weak var newIdeaButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func newideaButton(_ sender: Any) {
+        let startUpMaxIndex = UInt32(startup.count)
+        let subjectMaxIndex = UInt32(subject.count)
+        
+        let startUpRandomIndex = Int(arc4random_uniform(startUpMaxIndex))
+        let subjectRandomIndex = Int(arc4random_uniform(subjectMaxIndex))
+        
+        startUpLbl.text = startup[startUpRandomIndex]
+        subjectLbl.text = subject[subjectRandomIndex]
+    }
+    
 
 }
 
